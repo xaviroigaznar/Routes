81529ef77b323d704a9cfd36d96834f3b1bad97f
Xavi Roig Aznar <xaviroigaznar97@gmail.com>
2024-11-02 10:04:58 +0100

    Initial Commit

---
+++ Routes/RoutesApp.swift
@@ -0,0 +1,32 @@
+//
+//  RoutesApp.swift
+//  Routes
+//
+//  Created by Xavi Roig Aznar on 2/11/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RoutesApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
