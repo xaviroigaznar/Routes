4b49b55b97bc25c03957f873d2a6a3e5ba30e2d1
Xavi Roig Aznar <xaviroigaznar97@gmail.com>
2024-11-21 19:13:12 +0100

    Organize mapView

--- Routes/Map/MapView.swift
+++ Routes/Map/MapView.swift
@@ -35,39 +35,45 @@ struct MapView: View {
     @State private var mapStyleConfig = MapStyleConfig()
     @State private var pickMapStyle = false
 
-    var body: some View {
-        MapReader { proxy in
-            Map(position: $cameraPosition, selection: $selectedPlacemark) {
-                UserAnnotation()
-                ForEach(listPlacemarks, id: \.self) { placemark in
-                    if !showRoute {
-                        Group {
-                            if placemark.route != nil {
-                                Marker(coordinate: placemark.coordinate) {
-                                    Label(placemark.name, systemImage: "star")
-                                }
-                                .tint(.yellow)
-                            } else {
-                                Marker(placemark.name, coordinate: placemark.coordinate)
-                            }
-                        }.tag(placemark)
-                    } else {
-                        if let routeDestination {
-                            Marker(item: routeDestination)
-                                .tint(.green)
-                        }
+    // Track
+    @State private var showTrack = false
+
+    // Points of Interest
+    @State private var showPoisPicker = false
+
+    // Bindings
+    var isPlacemarkSelected: Binding<Bool> {
+        Binding(
+            get: { selectedPlacemark != nil },
+            set: { newValue in
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    if !newValue {
+                        selectedPlacemark = nil
                     }
                 }
-                if let route, routeDisplaying {
-                    MapPolyline(route.polyline)
-                        .stroke(.blue, lineWidth: 6)
+            }
+        )
+    }
+
+    var body: some View {
+        MapReader { proxy in
+            mapView
+            .alert("What do you want?", isPresented: isPlacemarkSelected) {
+                Button("Show track") {
+                    showTrack = true
+                }
+                Button("Search Points of Interest during the route") {
+                    showPoisPicker = true
+                }
+                Button("Dismiss", role: .cancel) {
+                    selectedPlacemark = nil
                 }
             }
-            .sheet(item: $selectedPlacemark) { selectedPlacemark in
+            .sheet(isPresented: $showTrack) {
                 RouteTrackView(selectedPlacemark: selectedPlacemark, cameraPosition: $cameraPosition)
                     .presentationDetents([.large])
             }
-            .onMapCameraChange{ context in
+            .onMapCameraChange { context in
                 visibleRegion = context.region
             }
             .onAppear {
@@ -95,135 +101,149 @@ struct MapView: View {
                 }
             }
             .safeAreaInset(edge: .bottom) {
-                HStack {
-                    VStack {
-                        TextField("Search...", text: $searchText)
-                            .textFieldStyle(.roundedBorder)
-                            .autocorrectionDisabled()
-                            .textInputAutocapitalization(.never)
-                            .focused($searchFieldFocus)
-                            .overlay(alignment: .trailing) {
-                                if searchFieldFocus {
-                                    Button {
-                                        searchText = ""
-                                        searchFieldFocus = false
-                                    } label: {
-                                        Image(systemName: "xmark.circle.fill")
-                                    }
-                                    .offset(x: -5)
-                                }
+                safeAreaInsetView
+            }
+        }
+    }
+}
+
+// MARK: - Inner Views
+private extension MapView {
+    @ViewBuilder var mapView: some View {
+        Map(position: $cameraPosition, selection: $selectedPlacemark) {
+            UserAnnotation()
+            ForEach(listPlacemarks, id: \.self) { placemark in
+                if !showRoute {
+                    Group {
+                        if placemark.route != nil {
+                            Marker(coordinate: placemark.coordinate) {
+                                Label(placemark.name, systemImage: "star")
                             }
-                            .onSubmit {
-                                Task {
-                                    await MapManager.searchPlaces(
-                                        modelContext,
-                                        searchText: searchText,
-                                        visibleRegion: visibleRegion
-                                    )
-                                    searchText = ""
-                                }
-                            }
-                        if routeDisplaying {
-                            HStack {
-                                Button("Clear Route", systemImage: "xmark.circle") {
-                                    removeRoute()
-                                }
-                                .buttonStyle(.borderedProminent)
-                                .fixedSize(horizontal: true, vertical: false)
-                                Button("Show Steps", systemImage: "location.north") {
-                                    showSteps.toggle()
-                                }
-                                .buttonStyle(.borderedProminent)
-                                .fixedSize(horizontal: true, vertical: false)
-                                .sheet(isPresented: $showSteps) {
-                                    if let route {
-                                        NavigationStack {
-                                            List {
-                                                HStack {
-                                                    Image(systemName: "mappin.circle.fill")
-                                                        .foregroundStyle(.red)
-                                                    Text("From my location")
-                                                    Spacer()
-                                                }
-                                                ForEach(1..<route.steps.count, id: \.self) { idx in
-                                                    VStack(alignment: .leading) {
-                                                        Text("Bike \(MapManager.distance(meters: route.steps[idx].distance))")
-                                                            .bold()
-                                                        Text(" - \(route.steps[idx].instructions)")
-                                                    }
-                                                }
-                                            }
-                                            .listStyle(.plain)
-                                            .navigationTitle("Steps")
-                                            .navigationBarTitleDisplayMode(.inline)
-                                        }
-                                    }
-                                }
-                            }
+                            .tint(.yellow)
+                        } else {
+                            Marker(placemark.name, coordinate: placemark.coordinate)
                         }
+                    }.tag(placemark)
+                } else {
+                    if let routeDestination {
+                        Marker(item: routeDestination)
+                            .tint(.green)
                     }
-                    .padding()
-                    VStack {
-                        if !searchPlacemarks.isEmpty {
-                            Button {
-                                MapManager.removeSearchResults(modelContext)
-                            } label: {
-                                Image(systemName: "mappin.slash")
-                            }
-                            .buttonStyle(.borderedProminent)
-                            .tint(.red)
-                        }
-                        Button {
-                            pickMapStyle.toggle()
-                        } label: {
-                            Image(systemName: "globe.americas.fill")
-                                .imageScale(.large)
-                        }
-                        .padding(8)
-                        .background(.thickMaterial)
-                        .clipShape(.circle)
-                        .sheet(isPresented: $pickMapStyle) {
-                            MapStyleView(mapStyleConfig: $mapStyleConfig)
-                                .presentationDetents([.height(275)])
-                        }
-                        MapUserLocationButton(scope: mapScope)
-                        MapCompass(scope: mapScope)
-                            .mapControlVisibility(.visible)
-                        MapPitchToggle(scope: mapScope)
-                            .mapControlVisibility(.visible)
-                    }
-                    .padding()
-                    .buttonBorderShape(.circle)
                 }
             }
-//                .onTapGesture { position in
-//                    if let pointedCoordinate = proxy.convert(position, from: .global) {
-//                        let coordinate = CLLocationCoordinate2D(latitude: pointedCoordinate.latitude,
-//                                                                longitude: pointedCoordinate.longitude)
-//                        self.position = MapCameraPosition.region(MKCoordinateRegion(center: coordinate,
-//                                                                                    span: MKCoordinateSpan(latitudeDelta: 0.1,
-//                                                                                                           longitudeDelta: 0.1)))
-//                        print(pointedCoordinate)
-//                        newCoordinate = coordinate
-//                    }
-//                }
+            if let route, routeDisplaying {
+                MapPolyline(route.polyline)
+                    .stroke(.blue, lineWidth: 6)
+            }
         }
-//        .onChange(of: position) { oldValue, newValue in
-//            showingAlert = true
-//        }
-//        .alert("Do you want to change the route?", isPresented: $showingAlert) {
-//            Button("Yes", role: .none) {
-//                if let newCoordinate {
-//                    routes.append(Route(name: "New route", latitude: newCoordinate.latitude, longitude: newCoordinate.longitude))
-//                }
-//                newCoordinate = nil
-//            }
-//            Button("No", role: .cancel) {
-//                newCoordinate = nil
-//            }
-//        }
     }
 
+    @ViewBuilder var safeAreaInsetView: some View {
+        HStack {
+            VStack {
+                TextField("Search...", text: $searchText)
+                    .textFieldStyle(.roundedBorder)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                    .focused($searchFieldFocus)
+                    .overlay(alignment: .trailing) {
+                        if searchFieldFocus {
+                            Button {
+                                searchText = ""
+                                searchFieldFocus = false
+                            } label: {
+                                Image(systemName: "xmark.circle.fill")
+                            }
+                            .offset(x: -5)
+                        }
+                    }
+                    .onSubmit {
+                        Task {
+                            await MapManager.searchPlaces(
+                                modelContext,
+                                searchText: searchText,
+                                visibleRegion: visibleRegion
+                            )
+                            searchText = ""
+                        }
+                    }
+                if routeDisplaying {
+                    HStack {
+                        Button("Clear Route", systemImage: "xmark.circle") {
+                            removeRoute()
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .fixedSize(horizontal: true, vertical: false)
+                        Button("Show Steps", systemImage: "location.north") {
+                            showSteps.toggle()
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .fixedSize(horizontal: true, vertical: false)
+                        .sheet(isPresented: $showSteps) {
+                            if let route {
+                                NavigationStack {
+                                    List {
+                                        HStack {
+                                            Image(systemName: "mappin.circle.fill")
+                                                .foregroundStyle(.red)
+                                            Text("From my location")
+                                            Spacer()
+                                        }
+                                        ForEach(1..<route.steps.count, id: \.self) { idx in
+                                            VStack(alignment: .leading) {
+                                                Text("Bike \(MapManager.distance(meters: route.steps[idx].distance))")
+                                                    .bold()
+                                                Text(" - \(route.steps[idx].instructions)")
+                                            }
+                                        }
+                                    }
+                                    .listStyle(.plain)
+                                    .navigationTitle("Steps")
+                                    .navigationBarTitleDisplayMode(.inline)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .padding()
+            VStack {
+                if !searchPlacemarks.isEmpty {
+                    Button {
+                        MapManager.removeSearchResults(modelContext)
+                    } label: {
+                        Image(systemName: "mappin.slash")
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.red)
+                }
+                Button {
+                    pickMapStyle.toggle()
+                } label: {
+                    Image(systemName: "globe.americas.fill")
+                        .imageScale(.large)
+                }
+                .padding(8)
+                .background(.thickMaterial)
+                .clipShape(.circle)
+                .sheet(isPresented: $pickMapStyle) {
+                    MapStyleView(mapStyleConfig: $mapStyleConfig)
+                        .presentationDetents([.height(275)])
+                }
+                MapUserLocationButton(scope: mapScope)
+                MapCompass(scope: mapScope)
+                    .mapControlVisibility(.visible)
+                MapPitchToggle(scope: mapScope)
+                    .mapControlVisibility(.visible)
+            }
+            .padding()
+            .buttonBorderShape(.circle)
+        }
+    }
+}
+
+// MARK: - Private methods
+private extension MapView {
     func updateCameraPosition() {
         if let userLocation = locationManager.userLocation {
             let userRegion = MKCoordinateRegion(
